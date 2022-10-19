83cdced8e4515f7cecd30873a25bdfb82f1adac4
lyq11 <springliu2003@gmail.com>
2022-10-18 21:29:24 -0700

    2

--- src/main/model/Alert.java
+++ src/main/model/Alert.java
@@ -1,28 +1,31 @@
 package model;
 
+import java.util.Date;
+
 // A class that includes all fields of a DueAlert
 public class Alert {
-    private String date;
+    private Date date;
     private int time;
     private String due;
     private int repeat;
 
     // Constructor method that creates a default Alert.
-    public Alert(String date, int time, String due, int repeat) {
+    public Alert(Date date, int time, String due, int repeat) {
         this.date = date;
         this.time = time;
         this.due = due;
         this.repeat = repeat;
     }
 
-    // REQUIRES: none
+
+    // REQUIRES: month has to be 1 to 12; days have to be 1-31
     // MODIFIES: this
     // EFFECTS: changes the date of the alert
-    public void changeDate(String s) {
-        date = s;
+    public Date changeDate(Date d) {
+        date = d;
     }
 
-    // REQUIRES: none
+    // REQUIRES: time is in between 0000 and 2359
     // MODIFIES: this
     // EFFECTS: changes the time of the alert
     public void changeTime(int time) {
@@ -36,14 +39,14 @@ public class Alert {
         this.due = due;
     }
 
-    // REQUIRES: none
+    // REQUIRES: repeat times >= 0
     // MODIFIES: this
     // EFFECTS: changes the name of the alert
     public void changeRepeat(int repeat) {
         this.repeat = repeat;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
--- src/test/model/TestAlert.java
+++ src/test/model/TestAlert.java
@@ -1,5 +1,23 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 class TestAlert {
-    // delete or rename this class!
+
+    Alert alertModel;
+    Date dateModel;
+
+    @BeforeEach
+    public void setup() {
+        dateModel = new Date();
+        alertModel = new Alert(dateModel 2359, "phase1", 1 );
+    }
+
+    @Test
+    public void testChangeDate() {
+        alertModel.changeDate();
+    }
 }
